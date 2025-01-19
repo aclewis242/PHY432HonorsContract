bc76ff7cc76159f492e2b1be319d3a258a81a78e
aclewis242 <aclewis242@gmail.com>
2025-01-19 14:24:49 -0700

    minor cleanup & revision to make the code easier to understand/read

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ### *Written report included*
 
 * To run the code and produce the results shown, simply run `main.py` in an activated conda environment.
-    - To change the radioactive source material, change line 36 (`eTup`) to `Ba` or `Cs` as desired
-    - To change the total number of photons used, change line 37 (`photonCount`) as desired
-    - To change the size of the angle bin used in the intensity calculations, change line 38 (`binSize`) as desired
+    - Cesium-137 and barium-133 are present by default, but more can be added in the same format
+    - Parameters are as listed in the `run` method
--- main.py
+++ main.py
@@ -8,56 +8,88 @@ A = 1/137.04  # Fine structure constant
 H = 4.136e-21 # Planck's constant (in MeV/Hz)
 C = 3e8       # Speed of light (in m/s)
 
-def compton(l, theta): # Compton's equation. Gives scattered wavelength from initial wavelength + scattering angle.
-    return L*(1 - np.cos(theta)) + l
+def compton(lam: float, theta: np.ndarray[float]) -> np.ndarray[float]:
+    '''
+    Compton's equation. Gives scattered wavelength from initial wavelength + scattering angle.
 
-def pdf(l, theta): # The Klein-Nishina formula, which serves as a probability density function in this case.
+    ### Parameters
+    - `lam`: Initial wavelength (m)
+    - `theta`: Scattering angle (rad). Accepts either a float or a NumPy array of floats
+    '''
+    return L*(1 - np.cos(theta)) + lam
+
+def pdf(lam: float, theta: np.ndarray[float]) -> np.ndarray[float]:
+    '''
+    The Klein-Nishina formula, which serves as a probability density function in this case.
+
+    ### Parameters
+    - `lam`: Initial wavelength (m)
+    - `theta`: Scattering angle (rad). Accepts either a float or a NumPy array of floats
+    '''
     coeff = (A**2*L**2)/(8*np.pi**2)
-    r = l/compton(l, theta)
+    r = lam/compton(lam, theta)
     term2 = r + 1/r - np.sin(theta)**2
     return coeff*r**2*term2
 
-def genRandAngle(l, thetas=np.linspace(0, np.pi, 1000)): # Generates and returns a random angle according to the above PDF.
-    probs = pdf(l, thetas)
-    return random.choices(thetas, probs)
+def genRandAngle(lam: float, thetas=np.linspace(0, np.pi, 1000)) -> float:
+    '''
+    Generates and returns a random angle according to the Klein-Nishina formula.
 
-def ELconv(le): # Converts between energy and wavelength.
-    return H*C/le
+    ### Parameters
+    - `lam`: Initial wavelength (m)
+    - `thetas`: Scattering angles (rad)
+    '''
+    probs = pdf(lam, thetas)
+    return random.choices(thetas, probs)[0]
 
-def linReg(ts, es): # Performs linear regression on the given angles and energies.
-    thetas_p = 1 - np.cos(ts)
-    energies_p = 1/es
-    return np.polyfit(thetas_p, energies_p, 1), thetas_p, energies_p
+def ELconv(lam_or_e: float):
+    '''
+    Converts between energy (MeV) and wavelength (m). Either quantity is a viable parameter. Accepts NP arrays.
+    '''
+    return H*C/lam_or_e
 
-if __name__ == "__main__":
-    Cs = (0.6617, "Cs-137") # Energy of gamma particles produced by cesium-137 decay (MeV)
-    Ba = (0.3560, "Ba-133") # " "   " " ...produced by barium-133 decay (MeV)
+def linReg(thetas: np.ndarray[float], es: np.ndarray[float]) -> tuple[np.ndarray[float], np.ndarray[float], np.ndarray[float]]:
+    '''
+    Performs linear regression on the given angles (rad) and energies (MeV).
 
-    ### CHANGE THESE LINES TO CHANGE PARAMETERS
-    eTup = Cs           # Source material
-    photonCount = 10000 # Number of photons
-    binSize = 6         # Size of angle bin (for intensity)
+    ### Returns
+    - 2-element array of the line's slope & intercept
+    - Linearised thetas (1 - cos(theta), x axis)
+    - Linearised energies (1/E, y axis)
+    '''
+    thetas_lin = 1 - np.cos(thetas)
+    energies_lin = 1/es
+    return np.polyfit(thetas_lin, energies_lin, 1), thetas_lin, energies_lin
 
-    e = eTup[0]
-    l = ELconv(e)
+def run(sourceMat: tuple[float, str], photonCount: int=10000, binSize: int=6):
+    '''
+    Runs the simulation.
+
+    ### Parameters
+    - `sourceMat`: A tuple of gamma particle energy (MeV) and source material name
+    - `photonCount`: The number of photons to simulate
+    - `binSize`: The size of the angle bin (intensity)
+    '''
+
+    e = sourceMat[0]
+    lam = ELconv(e)
     thetas = np.linspace(0, np.pi, 1000)
     intens = np.zeros_like(thetas)
     for n in range(1, photonCount):
-        t = genRandAngle(l, thetas)
-        angInd = np.where(thetas == t)[0]
+        t = genRandAngle(lam, thetas)
+        angInd = np.where(thetas == t)[0][0]
         st = angInd - angInd%binSize
-        st = st[0]
         intens[st:st + binSize] += 1
-    es = ELconv(compton(l, thetas))
+    es = ELconv(compton(lam, thetas))
     lr, ts_p, es_p = linReg(thetas, es)
     lr_line = lr[0]*ts_p + lr[1]
-    mOfE = 1/lr[0]
+    mOfE = 1/lr[0] # Electron mass
     
     # ENERGY VS ANGLE
     plt.plot(thetas, es)
-    plt.ylabel("Photon energy $E$ (MeV)")
+    plt.ylabel(r"Photon energy $E$ (MeV)")
     plt.xlabel(r"Angle $\theta$ (rad)")
-    plt.savefig("photonEnergy_{}.png".format(eTup[1]))
+    plt.savefig("photonEnergy_{}.png".format(sourceMat[1]))
     plt.show()
 
     # INTENSITY VS ANGLE
@@ -67,33 +99,15 @@ if __name__ == "__main__":
     ax1.set_xlabel(r"Angle $\theta$ (rad)")
     ax1.set_ylim(ymin=0)
     ax2 = ax1.twinx()
-    probsNoNorm = pdf(l, thetas)
+    probsNoNorm = pdf(lam, thetas)
     probs = probsNoNorm/np.linalg.norm(probsNoNorm)
     rsq = r2(intens/np.linalg.norm(intens), probs)
     ax2.plot(thetas, probs, color='r', label='Klein-Nishina formula ($R^2$: {:.4f})'.format(rsq))
     ax2.set_ylabel("Probability")
     ax2.set_ylim(ymin=0)
     plt.legend()
-    plt.savefig("photonIntensity_{}.png".format(eTup[1]))
+    plt.savefig("photonIntensity_{}.png".format(sourceMat[1]))
     plt.show()
-
-    ### This section has been deprecated -- there seemed to be some sort of correlation between energy & intensity when intensity was scaled
-    ### logarithmically, but after a few trials with different parameter values and scaling, I concluded that the two just happened to look
-    ### similar in this case
-    ### ENERGY, INTENSITY OVERLAY
-    # fig, ax1 = plt.subplots()
-    # ax1.scatter(thetas, intens)
-    # ax1.set_ylabel("Intensity (# photons)")
-    # ax1.set_yscale('log')
-    # ax1.set_ylim(ymin=0)
-    # ax2 = ax1.twinx()
-    # ax2.plot(thetas, es, color='r')
-    # ax2.set_ylabel("Photon energy $E$ (MeV)")
-    # ax2.set_xlabel(r"Angle $\theta$ (rad)")
-    # ax2.set_ylim(ymin=0)
-    # # plt.title("Energy, intensity overlaid")
-    # plt.savefig("energyIntensity_{}.png".format(eTup[1]))
-    # plt.show()
 
     # ELECTRON MASS LINEAR REGRESSION
     plt.scatter(ts_p, es_p)
@@ -101,5 +115,11 @@ if __name__ == "__main__":
     plt.ylabel(r"$\frac{1}{E}$ ($E$: scattered photon energy, MeV)")
     plt.xlabel(r"$1 - \cos(\theta)$ ($\theta$: scattering angle, rad)")
     plt.legend()
-    plt.savefig("electronMass_{}.png".format(eTup[1]))
+    plt.savefig("electronMass_{}.png".format(sourceMat[1]))
     plt.show()
+
+if __name__ == '__main__':
+    Cs = (0.6617, "Cs-137") # Energy of gamma particles produced by cesium-137 decay (MeV)
+    Ba = (0.3560, "Ba-133") # " "   " "              ...produced by barium-133 decay (MeV)
+
+    [run(mat) for mat in [Cs, Ba]]
